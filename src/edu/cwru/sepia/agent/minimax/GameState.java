7d5d30277b4fa6c67da81d49a8b6de09e4160e09
James Wright <12wrigja@gmail.com>
2016-03-01 11:42:02 -0500

    Turn now flip-flops correctly.

--- src/edu/cwru/sepia/agent/minimax/GameState.java
+++ src/edu/cwru/sepia/agent/minimax/GameState.java
@@ -281,6 +281,7 @@ public class GameState {
     private GameStateChild childFromStateWithAction(State.StateView state, Map<Integer, Action> unitActions) {
         GameState g = new GameState(state);
         g.computeUnitLists(this.myPlayerID);
+        g.isMyTurn = !isMyTurn;
         GameStateChild newChild = new GameStateChild(unitActions, g);
         unitActions.forEach((integer, action) -> {
             BetterUnit unit = newChild.state.allUnits.get(integer);
